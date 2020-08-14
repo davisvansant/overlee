f13b499812b9ec74e3e1201716f36f805b0d4006
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-08-14 15:16:57 -0700

    updated with initial greeting

--- overlee/src/main.rs
+++ overlee/src/main.rs
@@ -2,6 +2,8 @@
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = overlee_client::DiscographyClient::connect("http://[::]:8080").await?;
 
+    println!(" ~ Hello...");
+    println!(" ~ hello hello hello");
     overlee_client::get_albums(&mut client).await?;
     overlee_client::get_eps(&mut client).await?;
     overlee_client::get_singles(&mut client).await?;
