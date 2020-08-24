ba092078e3c6809b274ca882c8b3ad1103078d04
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-08-24 14:07:16 -0700

    some error handling, continues on invalid album/ep lookup

--- overlee/src/main.rs
+++ overlee/src/main.rs
@@ -10,18 +10,26 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!(" ~ hello hello hello");
     overlee_client::get_albums(&mut client).await?;
     println!(" ~ select album to lookup");
-    io::stdin()
-        .read_line(&mut get_album_id)
-        .expect("Failed to read line");
-    let parsed_album = get_album_id.trim().parse::<u32>().unwrap();
-    overlee_client::get_album(parsed_album, &mut client).await?;
-    println!(" ~ select ep to lookup");
+    io::stdin().read_line(&mut get_album_id)?;
+    let parsed_album = get_album_id.trim().parse::<u32>()?;
+    let get_album = overlee_client::get_album(parsed_album, &mut client).await;
+
+    match get_album {
+        Ok(response) => println!("{:?}", response),
+        Err(error) => println!("{:?}", error),
+    }
+
     overlee_client::get_eps(&mut client).await?;
-    io::stdin()
-        .read_line(&mut get_ep_id)
-        .expect("Failed to read line");
-    let parsed_ep = get_ep_id.trim().parse::<u32>().unwrap();
-    overlee_client::get_ep(parsed_ep, &mut client).await?;
+    println!(" ~ select ep to lookup");
+    io::stdin().read_line(&mut get_ep_id)?;
+
+    let parsed_ep = get_ep_id.trim().parse::<u32>()?;
+    let get_ep = overlee_client::get_ep(parsed_ep, &mut client).await;
+
+    match get_ep {
+        Ok(response) => println!("{:?}", response),
+        Err(error) => println!("{:?}", error),
+    }
     overlee_client::get_singles(&mut client).await?;
 
     Ok(())
